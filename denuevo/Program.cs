d6b309fdab2372ff424129ba5c44302976ecf88d
Latitude E5570 <Latitude E5570@DESKTOP-LGPMR19>
2023-08-28 16:40:37 -0600

    agruegue mas texto

--- denuevo/Program.cs
+++ denuevo/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, you esta es una prueba");
+Console.WriteLine("Hello, you esta es una prueba");
+Console.WriteLine("Hello, you esta es una prueba");
+Console.WriteLine("Hello, you esta es una prueba");
+Console.WriteLine("Hello, you esta es una prueba");
+Console.WriteLine("Hello, you esta es una prueba");
+Console.WriteLine("Hello, you esta es una prueba");
+Console.WriteLine("Hello, you esta es una prueba");

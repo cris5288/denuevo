88e3f60879f1854b9e6bf635bb0eba592fc02375
cris5288 <139468205+cris5288@users.noreply.github.com>
2023-08-28 16:42:07 -0600

    Borre toda esa verga

--- denuevo/Program.cs
+++ denuevo/Program.cs
@@ -1,10 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, you esta es una prueba");
-Console.WriteLine("Hello, you esta es una prueba");
-Console.WriteLine("Hello, you esta es una prueba");
-Console.WriteLine("Hello, you esta es una prueba");
-Console.WriteLine("Hello, you esta es una prueba");
-Console.WriteLine("Hello, you esta es una prueba");
-Console.WriteLine("Hello, you esta es una prueba");
-Console.WriteLine("Hello, you esta es una prueba");
-Console.WriteLine("Hello, you esta es una prueba");
+

c30f9b96553b33f310d72f09219b93c5f03e2153
Latitude E5570 <Latitude E5570@DESKTOP-LGPMR19>
2023-08-28 16:45:22 -0600

    modificado

--- denuevo/Program.cs
+++ denuevo/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, you esta es una prueba");
+Console.WriteLine("Hello, you esta es una prueba");
+Console.WriteLine("Hello, you esta es una prueba");
+Console.WriteLine("Hello, you esta es una prueba");
+Console.WriteLine("Hello, you esta es una prueba");
 
